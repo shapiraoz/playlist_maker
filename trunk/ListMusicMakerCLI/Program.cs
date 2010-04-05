f198f7d4772661593d795002983fc68777bf7a96
shapiraoz <shapiraoz@a8d39ecc-af94-bb5e-9d1f-f74d7b903021>
2010-04-05 18:13:30 +0000

    main core ready working with m3u files and wpl files

--- trunk/ListMusicMakerCLI/Program.cs
+++ trunk/ListMusicMakerCLI/Program.cs
@@ -12,16 +12,16 @@ namespace ListMusicMakerCLI
         
         static void Main(string[] args)
         {
-            //WplMake w1 = new WplMake(args[0]);
-            //w1.ServiceFunc = new ServiceFuncCLI();
-            //w1.ReadData();
-            //w1.CreateFolderOutPut(args[1]);
-            //w1.CopyAllFiles();
-            M3uMake m1 = new M3uMake(args[0]);
-            m1.ServiceFunc = new ServiceFuncCLI();
-            m1.ReadData();
-            m1.CreateFolderOutPut(args[1]);
-            m1.CopyAllFiles();
+            WplMake w1 = new WplMake(args[0]);
+            w1.ServiceFunc = new ServiceFuncCLI();
+            w1.ReadData();
+            w1.CreateFolderOutPut(args[1]);
+            w1.CopyAllFiles();
+            //M3uMake m1 = new M3uMake(args[0]);
+            //m1.ServiceFunc = new ServiceFuncCLI();
+            //m1.ReadData();
+            //m1.CreateFolderOutPut(args[1]);
+            //m1.CopyAllFiles();
         }
     }
 }
