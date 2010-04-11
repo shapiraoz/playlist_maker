4ab7af8822ce896608a49a40e8c183990ce21826
shapiraoz <shapiraoz@a8d39ecc-af94-bb5e-9d1f-f74d7b903021>
2010-04-11 19:38:17 +0000

    Continue with the Gui...only design

---
+++ ListMusicMaker/PlayListmakerApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreListMaker;
+
+namespace ListMusicMaker
+{
+    class PlayListmakerApp:PlayListMaker
+    {
+        public PlayListmakerApp(string path, string folderName)
+            : base(path, folderName)
+        {
+
+        }
+
+
+    }
+}
